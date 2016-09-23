c8168ea8bd3c5a06b2e0e2b33ba6cf3aacc83f10
Michael Sinnott <mesinnott@gmail.com>
2016-09-23 15:39:00 -0600

    player 1

--- game.js
+++ game.js
@@ -1,42 +1,56 @@
-var health = 100;
-var slap = 5;
-var punch = 10;
-var kick = 20;
-var hadouken = 35
+// var health = 100;
+// var slap = 5;
+// var punch = 10;
+// var kick = 20;
+// var hadouken = 35
+
+function Player (name, action1, action2, action3, special, health){
+    this.name = name;
+    this.action1 = 5;
+    this.action2 = 10;
+    this.action3 = 15;
+    this.special = 30;
+    this.health = 100;
+}
+
+var p1 = new Player('player 1', 'slap', 'punch', 'kick', 'hadouken', 100)
 
 function onSlap(){
-  health -= slap
+  p1.health -= p1.action1
   update()
-  return health
+  return p1.health
 }
 
 function onPunch(){
-  health -= punch
+  p1.health -= p1.action2
   update()
-  return health
+  return p1.health
 }
 
 function onKick(){
-    health-= kick
+    p1.health-= p1.action3
     update()
-    return health
+    return p1.health
     }
 
 function onHadouken(){
-    health -= hadouken
+    p1.health -= p1.special
     update()
-    return health
+    return p1.health
 }
 
 function reset(){
-    health = 100
+    p1.health = 100
     update()
 }
 
 function update(){
   var healthElem = document.getElementById('health')
-  if(health<=0){
-      health = 0
+  if(p1.health<=0){
+      p1.health = 0
+      document.getElementById('player-panel').classList.add('panel-danger')
+  }else{
+      document.getElementById('player-panel').classList.remove('panel-danger') 
   }
-  healthElem.innerHTML = health
+  healthElem.innerHTML = p1.health
 }
